3653a5ea07dd3e85d5239ad1328cf73cca109b3b
serba <serbinyo@gmail.com>
2018-12-02 01:39:45 +0300

    SQL washington_post task

---
+++ SQL/Washington_posts/max_comments.sql
@@ -0,0 +1,19 @@
+SELECT p.name, COUNT(c.id) as comments
+FROM post p
+JOIN comment c
+ON p.id = c.post_id
+GROUP BY p.id
+ORDER BY comments DESC
+LIMIT 1;
+
+SELECT p.name, COUNT(c.id) as comments
+FROM post p
+JOIN comment c
+ON p.id = c.post_id
+GROUP BY p.id
+HAVING comments = 
+(SELECT COUNT(*) as result
+FROM comment
+GROUP BY post_id
+ORDER BY result DESC
+LIMIT 1);
